b4e8f89cf3ef9e64ffb58021be3d4399fe54dac7
David-mwas <dmwas704@gmail.com>
2024-03-13 23:28:00 +0300

    Add : articles,story routes
    Co-authored-by: KariukiAntony  <antonygichoya9@gmail.com>

---
+++ routes/story.js
@@ -0,0 +1,19 @@
+const storyRouter = require("express").Router();
+
+const {
+  getAllStories,
+  getStoryById,
+  createStory,
+  deleteStory,
+} = require("../controllers/story");
+const { verifyAccessToken } = require("../helpers/getJwt");
+
+// Story routes
+storyRouter.get("/stories", getAllStories);
+storyRouter.get("/stories/:id", getStoryById);
+
+storyRouter.post("/stories", verifyAccessToken, createStory);
+storyRouter.put("/stories/:id", verifyAccessToken, updateStory);
+storyRouter.delete("/stories/:id", verifyAccessToken, deleteStory);
+
+module.exports = { storyRouter };
